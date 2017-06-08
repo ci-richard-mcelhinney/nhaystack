8d373cfd5eb99ce75f9a0c3ddf5bb4e709129c92
rmcelhinney <rmcelhinney@airmaster.com.au>
2017-06-08 14:11:43 +1000

    Added convenience methods for printing Dicts and Grids

--- test/src/nhaystack/test/NTest.java
+++ test/src/nhaystack/test/NTest.java
@@ -8,9 +8,11 @@
 package nhaystack.test;
 
 import java.lang.reflect.*;
+import java.util.*;
+
 import org.projecthaystack.*;
 import org.projecthaystack.client.*;
-import org.projecthaystack.test.*;
+// import org.projecthaystack.test.*;
 
 /**
  * Simple test harness to avoid pulling in dependencies.
@@ -49,6 +51,71 @@ public abstract class NTest extends Test
             System.out.println("verifyGridContains " + col + "=" + val + " failed!");
             fail();
         }
+    }
+    
+    /**
+     * Convenience to print and format a basic grid nicely for debugging
+     * 
+     * @param grid
+     */
+    void printBasicGrid(HGrid grid)
+    { 
+      int row = 0;
+      for (Iterator i = grid.iterator(); i.hasNext();)
+      {
+        HRow r = (HRow) i.next();
+        System.out.print(row + ", ");
+        System.out.print(r.id());
+        System.out.print("              ");
+        if (r.has("equipRef"))
+          System.out.println(r.get("equipRef"));
+        
+        System.out.print("              ");
+        if (r.has("kind"))          
+          System.out.println(r.get("kind"));
+        System.out.println();
+        row++;
+      }          
+    }
+    
+    /**
+     * Print a full grid
+     * 
+     * @param grid
+     */
+    void printFullGrid(HGrid grid)
+    {
+      int row = 0;
+      for (Iterator i = grid.iterator(); i.hasNext();)
+      {
+        HRow r = (HRow) i.next();
+        System.out.print(row + ", " + r.toString());
+        System.out.println();
+        row++;
+      }       
+    }
+
+    
+    /**
+     * Convenience to print and format a dict for debugging
+     * 
+     * @param dict
+     */
+    void printDict(HDict dict)
+    {           
+      for (Iterator i = dict.iterator(); i.hasNext();)
+      {
+        Map.Entry e = (Map.Entry) i.next();
+        String name = (String) e.getKey();
+        HVal val = (HVal) e.getValue();
+        if (name == "id" ||
+            name == "dis" ||
+            name == "equipRef") 
+        {        
+          System.out.print(name + ":" + val.toString() + " ");
+        }
+      }
+      System.out.println();
     }
 
     HDateTime ts(HDict r, String col) { return (HDateTime)r.get(col); }
