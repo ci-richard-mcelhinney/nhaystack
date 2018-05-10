825cc02ada2885daea4d6df28a2ae5b57aa7f5a7
Eric Anderson <eric.anderson3@tridium.com>
2018-05-10 13:21:02 -0400

    Migrating to slot annotations, adding missing @Overrides annotations, adding generics Rebuilt module-include.xml based on Niagara annotations

--- nhaystack-wb/src/nhaystack/ui/view/BNHaystackQueryView.java
+++ nhaystack-wb/src/nhaystack/ui/view/BNHaystackQueryView.java
@@ -3,65 +3,84 @@
 // Licensed under the Academic Free License version 3.0
 //
 // History:
-//   28 Apr 2013  Mike Jarmy  Creation
+//   28 Apr 2013  Mike Jarmy     Creation
+//   10 May 2018  Eric Anderson  Migrated to slot annotations, added missing @Overrides annotations
 //
 package nhaystack.ui.view;
 
-import java.util.*;
-
-import javax.baja.gx.*;
-import javax.baja.sys.*;
-import javax.baja.ui.*;
-import javax.baja.ui.event.*;
-import javax.baja.ui.options.*;
-import javax.baja.ui.pane.*;
-import javax.baja.ui.table.*;
-import javax.baja.ui.util.*;
-import javax.baja.util.*;
-import javax.baja.workbench.view.*;
-
-import org.projecthaystack.*;
-import nhaystack.*;
-import nhaystack.server.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import javax.baja.gx.BFont;
+import javax.baja.gx.BImage;
+import javax.baja.gx.BInsets;
+import javax.baja.gx.Graphics;
+import javax.baja.nre.annotations.AgentOn;
+import javax.baja.nre.annotations.NiagaraAction;
+import javax.baja.nre.annotations.NiagaraType;
+import javax.baja.sys.Action;
+import javax.baja.sys.BObject;
+import javax.baja.sys.BString;
+import javax.baja.sys.Context;
+import javax.baja.sys.Sys;
+import javax.baja.sys.Type;
+import javax.baja.ui.BLabel;
+import javax.baja.ui.BTextField;
+import javax.baja.ui.event.BWidgetEvent;
+import javax.baja.ui.options.BMruTextDropDown;
+import javax.baja.ui.pane.BBorderPane;
+import javax.baja.ui.pane.BEdgePane;
+import javax.baja.ui.table.BTable;
+import javax.baja.ui.table.TableCellRenderer;
+import javax.baja.ui.table.TableModel;
+import javax.baja.ui.util.BTitlePane;
+import javax.baja.util.Lexicon;
+import javax.baja.workbench.view.BWbView;
+import nhaystack.BHGrid;
+import nhaystack.server.BNHaystackService;
+import org.projecthaystack.HGrid;
+import org.projecthaystack.HMarker;
+import org.projecthaystack.HVal;
 
 /**
   * BNHaystackQueryView displays the Site-Equip-Point nav table.
   */
+@NiagaraType(
+  agent =   @AgentOn(
+    types = "nhaystack:NHaystackService"
+  )
+)
+@NiagaraAction(
+  name = "queryFilter",
+  parameterType = "BWidgetEvent",
+  defaultValue = "new BWidgetEvent()"
+)
 public class BNHaystackQueryView extends BWbView
 {
-    /*-
-    class BNHaystackQueryView
-    {
-        actions
-        {
-            queryFilter(event: BWidgetEvent) default {[ new BWidgetEvent() ]}
-        }
-    }
-    -*/
 /*+ ------------ BEGIN BAJA AUTO GENERATED CODE ------------ +*/
-/*@ $nhaystack.ui.view.BNHaystackQueryView(2576189528)1.0$ @*/
-/* Generated Sun Apr 28 10:49:50 EDT 2013 by Slot-o-Matic 2000 (c) Tridium, Inc. 2000 */
+/*@ $nhaystack.ui.view.BNHaystackQueryView(80306529)1.0$ @*/
+/* Generated Mon Nov 20 10:41:39 EST 2017 by Slot-o-Matic (c) Tridium, Inc. 2012 */
 
 ////////////////////////////////////////////////////////////////
 // Action "queryFilter"
 ////////////////////////////////////////////////////////////////
   
   /**
-   * Slot for the <code>queryFilter</code> action.
-   * @see nhaystack.ui.view.BNHaystackQueryView#queryFilter()
+   * Slot for the {@code queryFilter} action.
+   * @see #queryFilter(BWidgetEvent parameter)
    */
-  public static final Action queryFilter = newAction(0,new BWidgetEvent(),null);
+  public static final Action queryFilter = newAction(0, new BWidgetEvent(), null);
   
   /**
-   * Invoke the <code>queryFilter</code> action.
-   * @see nhaystack.ui.view.BNHaystackQueryView#queryFilter
+   * Invoke the {@code queryFilter} action.
+   * @see #queryFilter
    */
-  public void queryFilter(BWidgetEvent event) { invoke(queryFilter,event,null); }
+  public void queryFilter(BWidgetEvent parameter) { invoke(queryFilter, parameter, null); }
 
 ////////////////////////////////////////////////////////////////
 // Type
 ////////////////////////////////////////////////////////////////
   
+  @Override
   public Type getType() { return TYPE; }
   public static final Type TYPE = Sys.loadType(BNHaystackQueryView.class);
 
@@ -90,6 +109,7 @@ public class BNHaystackQueryView extends BWbView
         linkTo(filterMru.getEditor(), BTextField.actionPerformed, queryFilter);  
     }
 
+    @Override
     protected void doLoadValue(BObject value, Context cx)
     {
         this.service = (BNHaystackService) value;
@@ -120,7 +140,7 @@ public class BNHaystackQueryView extends BWbView
         }
     }
 
-    class GridModel extends TableModel
+    static class GridModel extends TableModel
     {
         GridModel(HGrid grid) 
         { 
@@ -131,30 +151,35 @@ public class BNHaystackQueryView extends BWbView
             for (int i = 0; i < grid.numCols(); i++)
                 indexNames[i] = new IndexName(i, grid.col(i).name());
 
-            Arrays.sort(indexNames, new Comparator() {
-                public int compare(Object o1, Object o2) {
-                    IndexName c1 = (IndexName) o1;
-                    IndexName c2 = (IndexName) o2;
-
-                    if (c1.name.equals("id")) return -1;
-                    else if (c2.name.equals("id")) return 1;
-                    else return (c1.name.compareTo(c2.name));
-            }});
+            Arrays.sort(indexNames, new Comparator<IndexName>()
+            {
+                @Override
+                public int compare(IndexName o1, IndexName o2)
+                {
+                    if (o1.name.equals("id")) return -1;
+                    else if (o2.name.equals("id")) return 1;
+                    else return o1.name.compareTo(o2.name);
+                }
+            });
 
             this.columnIndex = new int[indexNames.length];
             for (int i = 0; i < indexNames.length; i++)
                 columnIndex[i] = indexNames[i].index;
         }
 
+        @Override
         public int getRowCount() { return grid.numRows(); }
 
+        @Override
         public int getColumnCount() { return grid.numCols(); }
 
-        public String getColumnName(int col) 
+        @Override
+        public String getColumnName(int col)
         { 
             return grid.col(columnIndex[col]).name(); 
         }
 
+        @Override
         public Object getValueAt(int row, int col)
         {
             HVal val = grid.row(row).get(grid.col(columnIndex[col]), false);
@@ -178,8 +203,9 @@ public class BNHaystackQueryView extends BWbView
         private final String name;
     }
 
-    class GridCellRenderer extends TableCellRenderer
+    static class GridCellRenderer extends TableCellRenderer
     {
+        @Override
         public void paintCell(Graphics g, Cell cell)
         {
             if (cell.value instanceof HMarker)
@@ -202,9 +228,9 @@ public class BNHaystackQueryView extends BWbView
 ////////////////////////////////////////////////////////////////
 
     private static final Lexicon LEX = Lexicon.make("nhaystack");
-    private static BImage CHECK = BImage.make("module://icons/x16/check.png");
-    private static BFont BOLD = BFont.make("Tahoma", 11.0, BFont.BOLD);
-    private static String MRU = "nhaystack_filter";
+    private static final BImage CHECK = BImage.make("module://icons/x16/check.png");
+    private static final BFont BOLD = BFont.make("Tahoma", 11.0, BFont.BOLD);
+    private static final String MRU = "nhaystack_filter";
  
     private final BMruTextDropDown filterMru;
     private final BTitlePane title;
