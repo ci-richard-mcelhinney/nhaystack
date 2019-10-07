c58ea9c0fff040d6614d3390933f0ae3a36fc6ab
Richard McElhinney <richard.mcelhinney@conserveit.com.au>
2019-10-07 21:01:11 +1100

    Step 3, refactored writing to a string writable

--- nhaystack-rt/srcTest/nhaystack/server/BPointIOTest.java
+++ nhaystack-rt/srcTest/nhaystack/server/BPointIOTest.java
@@ -10,19 +10,15 @@ package nhaystack.server;
 
 import org.projecthaystack.HBool;
 import org.projecthaystack.HNum;
+import org.projecthaystack.HStr;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import javax.baja.control.BBooleanWritable;
-import javax.baja.control.BControlPoint;
-import javax.baja.control.BNumericWritable;
+import javax.baja.control.*;
 import javax.baja.control.enums.BPriorityLevel;
 import javax.baja.nre.annotations.NiagaraType;
-import javax.baja.status.BStatus;
-import javax.baja.status.BStatusNumeric;
-import javax.baja.sys.BBoolean;
-import javax.baja.sys.Sys;
-import javax.baja.sys.Type;
+import javax.baja.status.*;
+import javax.baja.sys.*;
 import javax.baja.test.BTestNg;
 
 import org.projecthaystack.HVal;
@@ -56,7 +52,7 @@ public class BPointIOTest extends BTestNg
         PointIO.writeNW(tp, BPriorityLevel.level_10, HNum.make(100d));
         Assert.assertEquals(tp.getIn10().getStatus(), BStatus.ok);
         Assert.assertEquals(tp.getIn10().getValue(), 100d);
-        verifyNWPointArrayStatus(tp, BPriorityLevel.level_10, BStatus.nullStatus);
+        verifyPointArrayStatus(tp, BPriorityLevel.level_10, BStatus.nullStatus);
 
         PointIO.writeNW(tp, BPriorityLevel.level_10, null);
         Assert.assertEquals(tp.getIn10().getStatus(), BStatus.nullStatus);
@@ -64,6 +60,7 @@ public class BPointIOTest extends BTestNg
         PointIO.writeNW(tp, BPriorityLevel.level_16, HNum.make(100d));
         Assert.assertEquals(tp.getIn16().getStatus(), BStatus.ok);
         Assert.assertEquals(tp.getIn16().getValue(), 100d);
+        verifyPointArrayStatus(tp, BPriorityLevel.level_16, BStatus.nullStatus);
 
         PointIO.writeNW(tp, BPriorityLevel.level_16, null);
         Assert.assertEquals(tp.getIn16().getStatus(), BStatus.nullStatus);
@@ -71,6 +68,7 @@ public class BPointIOTest extends BTestNg
         PointIO.writeNW(tp, BPriorityLevel.fallback, HNum.make(100d));
         Assert.assertEquals(tp.getFallback().getStatus(), BStatus.ok);
         Assert.assertEquals(tp.getFallback().getValue(), 100d);
+        verifyPointArrayStatus(tp, BPriorityLevel.none, BStatus.nullStatus);
     }
 
     @Test
@@ -84,6 +82,7 @@ public class BPointIOTest extends BTestNg
        PointIO.writeBW(tp, BPriorityLevel.level_10, HBool.make(true));
        Assert.assertEquals(tp.getIn10().getStatus(), BStatus.ok);
        Assert.assertTrue(tp.getIn10().getValue());
+       verifyPointArrayStatus(tp, BPriorityLevel.level_10, BStatus.nullStatus);
 
        PointIO.writeBW(tp, BPriorityLevel.level_10, null);
        Assert.assertEquals(tp.getIn10().getStatus(), BStatus.nullStatus);
@@ -91,6 +90,7 @@ public class BPointIOTest extends BTestNg
        PointIO.writeBW(tp, BPriorityLevel.level_16, HBool.make(true));
        Assert.assertEquals(tp.getIn16().getStatus(), BStatus.ok);
        Assert.assertTrue(tp.getIn16().getValue());
+       verifyPointArrayStatus(tp, BPriorityLevel.level_16, BStatus.nullStatus);
 
        PointIO.writeBW(tp, BPriorityLevel.level_16, null);
        Assert.assertEquals(tp.getIn16().getStatus(), BStatus.nullStatus);
@@ -98,12 +98,53 @@ public class BPointIOTest extends BTestNg
        PointIO.writeBW(tp, BPriorityLevel.fallback, HBool.make(true));
        Assert.assertEquals(tp.getFallback().getStatus(), BStatus.ok);
        Assert.assertTrue(tp.getFallback().getValue());
+       verifyPointArrayStatus(tp, BPriorityLevel.none, BStatus.nullStatus);
     }
 
     @Test
     public void testWriteEW()
     {
+//        BEnumWritable tp = new BEnumWritable();
+//
+//        PointIO.writeEW(tp, BPriorityLevel.level_10, null);
+//        Assert.assertEquals(tp.getIn10().getStatus(), BStatus.nullStatus);
+//
+//        PointIO.writeEW(tp, BPriorityLevel.level_10, H);
+    }
 
+    @Test
+    public void testWriteSW()
+    {
+        BStringWritable tp = new BStringWritable();
+
+        PointIO.writeSW(tp, BPriorityLevel.level_10, null);
+        Assert.assertEquals(tp.getIn10().getStatus(), BStatus.nullStatus);
+
+        PointIO.writeSW(tp, BPriorityLevel.level_10, HStr.make("hellow world"));
+        Assert.assertEquals(tp.getIn10().getStatus(), BStatus.ok);
+        verifyPointArrayStatus(tp, BPriorityLevel.level_10, BStatus.nullStatus);
+
+        PointIO.writeSW(tp, BPriorityLevel.level_10, null);
+        Assert.assertEquals(tp.getIn10().getStatus(), BStatus.nullStatus);
+
+        PointIO.writeSW(tp, BPriorityLevel.level_16, HStr.make("hellow world"));
+        Assert.assertEquals(tp.getIn16().getStatus(), BStatus.ok);
+        verifyPointArrayStatus(tp, BPriorityLevel.level_16, BStatus.nullStatus);
+
+        PointIO.writeSW(tp, BPriorityLevel.level_16, null);
+        Assert.assertEquals(tp.getIn10().getStatus(), BStatus.nullStatus);
+
+        PointIO.writeSW(tp, BPriorityLevel.fallback, HStr.make("hellow world"));
+        Assert.assertEquals(tp.getFallback().getStatus(), BStatus.ok);
+        verifyPointArrayStatus(tp, BPriorityLevel.none, BStatus.nullStatus);
+    }
+
+    private void verifyPointArrayStatus(BControlPoint p, BPriorityLevel skip, BStatus st)
+    {
+        if      (p instanceof BNumericWritable)  verifyNWPointArrayStatus((BNumericWritable) p, skip, st);
+        else if (p instanceof BBooleanWritable)  verifyBWPointArrayStatus((BBooleanWritable) p, skip, st);
+        else if (p instanceof BEnumWritable)     verifyEWPointArrayStatus((BEnumWritable) p, skip, st);
+        else if (p instanceof BStringWritable)   verifySWPointArrayStatus((BStringWritable) p, skip, st);
     }
 
     /**
@@ -131,6 +172,57 @@ public class BPointIOTest extends BTestNg
         }
     }
 
+    private void verifyBWPointArrayStatus(BBooleanWritable bw, BPriorityLevel skip, BStatus st)
+    {
+        BPriorityLevel curLevel;
+        BStatusBoolean sb;
 
+        for (int i = 1; i < 17; i++)
+        {
+            curLevel = BPriorityLevel.make(i);
+            if (curLevel == skip)
+            {
+                continue;
+            }
 
+            sb = bw.getLevel(curLevel);
+            Assert.assertEquals(sb.getStatus(), st);
+        }
+    }
+
+    private void verifyEWPointArrayStatus(BEnumWritable ew, BPriorityLevel skip, BStatus st)
+    {
+        BPriorityLevel curLevel;
+        BStatusEnum se;
+
+        for (int i = 1; i < 17; i++)
+        {
+            curLevel = BPriorityLevel.make(i);
+            if (curLevel == skip)
+            {
+                continue;
+            }
+
+            se = ew.getLevel(curLevel);
+            Assert.assertEquals(se.getStatus(), st);
+        }
+    }
+
+    private void verifySWPointArrayStatus(BStringWritable sw, BPriorityLevel skip, BStatus st)
+    {
+        BPriorityLevel curLevel;
+        BStatusString ss;
+
+        for (int i = 1; i < 17; i++)
+        {
+            curLevel = BPriorityLevel.make(i);
+            if (curLevel == skip)
+            {
+                continue;
+            }
+
+            ss = sw.getLevel(curLevel);
+            Assert.assertEquals(ss.getStatus(), st);
+        }
+    }
 }
