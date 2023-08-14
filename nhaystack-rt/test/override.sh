e4c38ab87f8e11abd76c28af391dcd7dc763a055
Richard McElhinney <richard.mcelhinney@conserveit.com.au>
2023-08-14 15:36:25 +1000

    Test scripts for issue #22 from Github issue tracker

--- nhaystack-rt/test/override.sh
+++ nhaystack-rt/test/override.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
+# Script to use from bash command line to test the override function on a
+# NumericWritable in a Niagara Station.  Script must have executable
+# permissions set on your file system, e.g.
+#
+#    chmod a+x override.sh
+#
+# before trying to run.
+#
+# You may need to customise the URL and 'id' as necessary to run on your system.
+#
+# For this script to work you will need to configure your Niagara Station to use
+# HTTP instead of HTTPS and you will need to have a user configured for Basic
+# Authentication.  The username and password need to be filled in at the appropriate
+# location in the commands below.
+#
+# N.B.  THIS SCRIPT IS FOR USE ON LOCAL NETWORKS ONLY, DO NOT USE ON PRODUCTION
+#       PRODUCTION SYSTEMS THAT ARE EXPOSED OVER THE INTERNET
+
+
+
 curl --location --request POST 'http://localhost/haystack/invokeAction' \
 -H "Content-Type: text/zinc; charset=utf-8" \
 --user "<your_user>:<your_password>" \
@@ -15,5 +35,5 @@ curl --location --request POST 'http://localhost/haystack/invokeAction' \
 --user "<your_user>:<your_password>" \
 --verbose \
 --data-raw 'ver:"3.0" action:"auto" id:@C.Apps.NumericWritable
-eempty
+empty
 N'
